c2982d5971e44c4bfab5bcec264055175900406d
Francisco Hanlly da Silva <hanlly.silvae@gmail.com>
2024-10-04 14:21:19 -0300

    update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const http = require('http');
 const hostname = '0.0.0.0';
 const port = 3000;
 
-const birthdayMessage = "<h1>OLÁ A TODOS QUE ESTÃO LENDO! TUDO BOM?</h1> ";
+const birthdayMessage = "<h1>OLÁ A TODOS QUE ESTÃO LENDO! TUDO BOM?????</h1> ";
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
